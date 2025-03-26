981968c9dc7bafeb07f8029361e51b529e71000a
estudiante <profedanieljuan@gmail.com>
2025-03-26 19:56:37 -0300

    Se actualiza la clase Snack

--- Snack.java
+++ Snack.java
@@ -1,2 +1,14 @@
-public class Snack {
+import java.io.Serializable;
+
+public class Snack implements Serializable {
+    private static int contadorSnack = 0;
+    private int idSnack;
+    private String nombre;
+    private double precio;
+
+    public Snack(){
+        this.idSnack = ++Snack.contadorSnack;
+        
+    }
+
 }
