b55fc82e97cbd836c7bbc144f6146fcf84049d93
estudiante <profedanieljuan@gmail.com>
2025-03-30 19:26:14 -0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Snack.java

--- Snack.java
+++ Snack.java
@@ -1,4 +1,5 @@
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Snack implements Serializable {
     private static int contadorSnack = 0;
@@ -20,4 +21,48 @@ public class Snack implements Serializable {
         this.precio = precio;
     }
 
+    public static int getContadorSnack() {
+        return contadorSnack;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdSnack() {
+        return idSnack;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "idSnack=" + idSnack +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return idSnack == snack.idSnack && Double.compare(precio, snack.precio) == 0 && Objects.equals(nombre, snack.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idSnack, nombre, precio);
+    }
 }
