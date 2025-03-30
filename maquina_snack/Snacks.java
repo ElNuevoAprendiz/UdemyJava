bc90c8ea71f3e5ffb093a30b294bc8fb4319c868
estudiante <profedanieljuan@gmail.com>
2025-03-30 19:57:10 -0300

    Se agrega el archivo Apuntes conceptuales, donde se hace un resumen de los conceptos que se van aprendiendo.
    Llamada a un constructor interno en la clase snack (la cual siempre debe ser la primera linea del constructor que lo llama.
    
    Uso de bloque estático para inicializar un elemento estático, en este caso una lista en la clase Snacks, ya que como en esta clase se va a trabajar solo con métodos estáticos y la lista que se usa tambien se la creo como un elemento estático no se usa constructor, y por esto para declararla se utiliza un bloque estático.

--- maquina_snack/Snacks.java
+++ maquina_snack/Snacks.java
@@ -5,5 +5,7 @@ import java.util.List;
 public class Snacks {
     private static final List<Snack> snacks;
 
-
+    //Bloque de tipo estático inicalizador para iniciar elementos estáticos en vez de usar un constructor
+    //Cuando se cargue la clase se van a crear las elementos estáticos y se van a inicializar al ejecutar este
+    //bloque estático.
 }
