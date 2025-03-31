ffefac7abd327b931d3cb208ce3a07d0866495e0
estudiante <profedanieljuan@gmail.com>
2025-03-30 22:56:28 -0300

    Se sigue completando la clase MaquinaSnacks

--- maquina_snack/MaquinaSnacks.java
+++ maquina_snack/MaquinaSnacks.java
@@ -16,10 +16,56 @@ public class MaquinaSnacks {
         List<Snack> productos = new ArrayList<>();
         System.out.println("***Máquina de Snack***");
         Snacks.mostrarSnack(); //Mostramos el inventario
+        while (!salir){
+            try{
+                var opcion = mostrarMenu(consola);
+                salir =  ejecutarOpciones(opcion, consola, productos);
 
-        // y luego escribimos un bloque estático para inicializar
-        // los atributos estáticos.
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error:" + e.getMessage());
+            }
+            finally {
+                System.out.println();//Imprime un alto de linea con cada iteración.
+            }
+        }
+
+
+    }
+
+    private static int mostrarMenu(Scanner consola){
+        System.out.print("""
+                Menu:
+                1. Comprar snack
+                2. Mostrar ticket
+                Agregar Nuevo Snack
+                4. Salir
+                
+                Elige una opción \s""");
+
+        //Leemos y retornamos la opcion elegida por el usuario
+        return Integer.parseInt(consola.nextLine());
+    }
+
+    private static boolean ejecutarOpciones(int opcion, Scanner consola, List<Snack> productos){
+        var salir = false;
+        switch (opcion){
+            case 1 -> comprarSnack(consola, productos);
+        }
+
+        return salir;
+
+    };
+
+    private static void comprarSnack(Scanner consola, List<Snack> productos){
+        System.out.println("Que snack quieres comprar (id)?");
+        var idSnack = Integer.parseInt(consola.nextLine());
+        //Validadr q el snack exista en la lista
+        var snackEncontrado = False;
+        for(var snack: Snacks.getSnacks()){
+
+        }
 
 
     }
 }
+
