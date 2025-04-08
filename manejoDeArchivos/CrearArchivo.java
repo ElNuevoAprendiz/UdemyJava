a21ff00a1ccc70a96ec876fa7eedaf96bc36317e
ElNuevoAprendiz! <profedanieljuan@gmail.com>
2025-04-08 09:50:30 -0300

    Se actualiza ApuntesConceptuales.html con las clases File, PrintWriter y FileWriter

--- manejoDeArchivos/CrearArchivo.java
+++ manejoDeArchivos/CrearArchivo.java
@@ -8,13 +8,13 @@ import java.io.PrintWriter;
 public class CrearArchivo {
     public static void main(String[] args) {
 
-        var nombreArchivo = "mi_archivo.txt";
-        /*************************************************************************************************
-        Creación de un objeto File:
 
-        Un objeto File representa una ruta de acceso a un archivo o directorio en el sistema de archivos.
-        Para crear un objeto File, puedes proporcionar la ruta como una cadena.
-        **************************************************************************************************/
+
+        ///***********************************************************************************************
+        ///Creación de un objeto File:
+        ///Un objeto File representa una ruta de acceso a un archivo o directorio en el sistema de archivos.
+        ///Para crear un objeto File, puedes proporcionar la ruta como una cadena.
+        var nombreArchivo = "mi_archivo.txt";
 
         var archivo = new File(nombreArchivo);
 
